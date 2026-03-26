7c04ae2b2234fe50be83c440964e881bc70d1f7a
Prashant Kumar <92239848+pkaws0908@users.noreply.github.com>
2026-03-26 14:47:54 +0530

    Update provider.tf

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "4.42.0"
     }
   }
+  backend "azurerm" {
+    use_cli              = true                                   
+    use_azuread_auth     = true                                   
+    tenant_id            = "01243cc1-e80d-488d-9e34-c0c5f65a9974" 
+    storage_account_name = "stgaccount1290"                              
+    container_name       = "tfstate"                              
+    key                  = "dev.terraform.tfstate"               
+  }
 }
 
 provider "azurerm" {
@@ -15,12 +23,5 @@ provider "azurerm" {
     }
   }
   subscription_id = "b45fb524-5427-4292-b739-56b83ea60765"
-  backend "azurerm" {
-    use_cli              = true                                   
-    use_azuread_auth     = true                                   
-    tenant_id            = "01243cc1-e80d-488d-9e34-c0c5f65a9974" 
-    storage_account_name = "stgaccount1290"                              
-    container_name       = "tfstate"                              
-    key                  = "dev.terraform.tfstate"               
-  }
+
 }
