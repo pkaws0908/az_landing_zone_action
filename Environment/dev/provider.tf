1283342cc28b9d5ef2cdb4cb13df7750dc6175a5
Prashant Kumar <92239848+pkaws0908@users.noreply.github.com>
2026-03-26 14:30:53 +0530

    Update provider.tf

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -14,5 +14,5 @@ provider "azurerm" {
       recover_soft_deleted_key_vaults = true
     }
   }
-  subscription_id = "8a5b9680-df5b-4e27-a4a2-ea7f28c1331c"
+  subscription_id = "b45fb524-5427-4292-b739-56b83ea60765"
 }
